481cc392f9dc530fc57c71c923a6c8519e60ff77
Frobeniusnorm <david.schwarzbeck@fau.de>
2021-11-28 23:26:48 +0100

    started interpreter (done nothing yet)

---
+++ src/main/scala/Interpreter.scala
@@ -0,0 +1,4 @@
+object Interpreter:
+    def interpret(prog:Program) = ???
+    //idea: per iteration try from outer to inner to match axioms
+    //if in one iteration not a single axiom was matched => terminate 
