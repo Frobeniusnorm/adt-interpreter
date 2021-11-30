9a49d04ed0928310a462e255fe5d4d70a7699e02
david <david.schwarzbeck@fau.de>
2021-11-30 17:17:27 +0100

    fixed main method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,14 +5,10 @@ def printHelp: Unit =
 
 def readFile:String => Array[String] = name => Source.fromFile(name).getLines.toArray
 @main
-def main: Array[String] => Unit = args => args match
-  case Array() => printHelp
-  case x => 
-    //parse compiler options
-    if x.length == 1 then
-      val ast = new AST(readFile(x(0)))
-      val np = Parser.parseProgram(ast.program)
-      val interpreter = new Interpreter(np)
-      (ast.program.expr zip interpreter.evaledExpr) foreach (
-        (x, y) => println(x.toString + " = " + y.toString)
-      )
+def main(file:String) =
+  val ast = new AST(readFile(file))
+  val np = Parser.parseProgram(ast.program)
+  val interpreter = new Interpreter(np)
+  (ast.program.expr zip interpreter.evaledExpr) foreach (
+    (x, y) => println(x.toString + " = " + y.toString)
+  )
